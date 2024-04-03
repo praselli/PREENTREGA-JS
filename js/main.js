89a07749a40261a789bbec816d7e9f0503214cef
PABLO RASELLI <116608564+praselli@users.noreply.github.com>
2024-04-03 11:47:56 -0300

    34º guardado

--- js/main.js
+++ js/main.js
@@ -170,7 +170,7 @@
 
 // let palabraIngresada = toLoerCase( prompt ("Ingrese una palabra"));
 
-const vocales = { a:0, e:0,  i:0, o:0, u:0 };
+const vocales = {"a":0, "e":0,  "i":0, "o":0, "u":0 };
 
 console.log(vocales);
 
